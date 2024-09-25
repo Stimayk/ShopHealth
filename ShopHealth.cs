e50a80b0b0c22f3b13c084f740e63a634ae572b3
Stimayk <51941742+Stimayk@users.noreply.github.com>
2024-09-25 10:21:10 +0400

    Update ShopHealth.cs

--- ShopHealth.cs
+++ ShopHealth.cs
@@ -7,13 +7,12 @@ using ShopAPI;
 
 namespace ShopHealth
 {
-    // [MinimumApiVersion(160)]
     public class ShopHealth : BasePlugin
     {
         public override string ModuleName => "[SHOP] Health";
         public override string ModuleDescription => "";
         public override string ModuleAuthor => "E!N";
-        public override string ModuleVersion => "v1.0.0";
+        public override string ModuleVersion => "v1.0.1";
 
         private IShopApi? SHOP_API;
         private const string CategoryName = "Health";
@@ -66,7 +65,7 @@ namespace ShopHealth
             RegisterListener<Listeners.OnClientDisconnect>(playerSlot => playerHealth[playerSlot] = null!);
         }
 
-        public void OnClientBuyItem(CCSPlayerController player, int itemId, string categoryName, string uniqueName, int buyPrice, int sellPrice, int duration, int count)
+        public HookResult OnClientBuyItem(CCSPlayerController player, int itemId, string categoryName, string uniqueName, int buyPrice, int sellPrice, int duration, int count)
         {
             if (TryGetNumberOfHealth(uniqueName, out int Health))
             {
@@ -76,9 +75,10 @@ namespace ShopHealth
             {
                 Logger.LogError($"{uniqueName} has invalid or missing 'health' in config!");
             }
+            return HookResult.Continue;
         }
 
-        public void OnClientToggleItem(CCSPlayerController player, int itemId, string uniqueName, int state)
+        public HookResult OnClientToggleItem(CCSPlayerController player, int itemId, string uniqueName, int state)
         {
             if (state == 1 && TryGetNumberOfHealth(uniqueName, out int Health))
             {
@@ -88,11 +88,13 @@ namespace ShopHealth
             {
                 OnClientSellItem(player, itemId, uniqueName, 0);
             }
+            return HookResult.Continue;
         }
 
-        public void OnClientSellItem(CCSPlayerController player, int itemId, string uniqueName, int sellPrice)
+        public HookResult OnClientSellItem(CCSPlayerController player, int itemId, string uniqueName, int sellPrice)
         {
             playerHealth[player.Slot] = null!;
+            return HookResult.Continue;
         }
 
         [GameEventHandler]
@@ -119,7 +121,7 @@ namespace ShopHealth
             Server.NextFrame(() => Utilities.SetStateChanged(playerPawn, "CBaseEntity", "m_iHealth"));
         }
 
-        private bool TryGetNumberOfHealth(string uniqueName, out int Health)
+        private static bool TryGetNumberOfHealth(string uniqueName, out int Health)
         {
             Health = 0;
             if (JsonHealth != null && JsonHealth.TryGetValue(uniqueName, out var obj) && obj is JObject jsonItem && jsonItem["health"] != null && jsonItem["health"]!.Type != JTokenType.Null)
